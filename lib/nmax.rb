7096b9339b5d38fa0886b9fd304136b0cc84af07
Avtor022 <avtor022@gmail.com>
2018-09-27 14:32:20 +1100

    error handling added: n is non-integer, no numbers

--- lib/nmax.rb
+++ lib/nmax.rb
@@ -7,17 +7,25 @@ module Nmax
     puts self.show_numbers(number_of_numbers, data)
   end
 
-  def self.show_numbers(n, data)
-    numbers = []
-    data.each_line do |line|
-      if matches = line.scan(/\d{1,1000}/)
-        matches.each do |match|
-          number = match.to_i
-          numbers << number
+  def self.show_numbers(n = 5, data)
+    if n.is_a? Integer
+      numbers = []
+      data.each_line do |line|
+        if matches = line.scan(/\d{1,1000}/)
+          matches.each do |match|
+            number = match.to_i
+            numbers << number
+          end
         end
       end
+      if numbers == []
+        message = "No numbers!"
+      else
+        numbers.max(n)
+      end
+    else
+      message = "Error: parameter 'number' must be Integer!"
     end
-    numbers.sort.reverse[0..n-1]
   end
 
 end
--- spec/nmax_spec.rb
+++ spec/nmax_spec.rb
@@ -3,18 +3,29 @@ RSpec.describe Nmax do
     expect(Nmax::VERSION).not_to be nil
   end
 
-  let(:test_file_path) { File.dirname(__FILE__) + '/demo/test.txt' }
-  let(:text) { File.open(test_file_path) }
+  let(:test_with_numbers_path) { File.dirname(__FILE__) + '/demo/test_with_numbers.txt' }
+  let(:text_with_numbers) { File.open(test_with_numbers_path) }
+  let(:test_without_numbers_path) { File.dirname(__FILE__) + '/demo/test_without_numbers.txt' }
+  let(:text_without_numbers) { File.open(test_without_numbers_path) }
 
   it ".show_numbers shows an array with 5 integers" do
-    expect(subject.show_numbers(5, text).all? { |a| a.class == Fixnum }).to be_truthy
+    expect(subject.show_numbers(5, text_with_numbers).all? { |a| a.class == Fixnum }).to be_truthy
   end
 
   it ".show_numbers shows an array with 3 elements" do
-    expect(subject.show_numbers(3, text).count).to eq(3)
+    expect(subject.show_numbers(3, text_with_numbers).count).to eq(3)
   end
 
   it ".show_numbers correctly shows integers from text" do
-    expect(subject.show_numbers(2, text)).to eq([999999999999999999999999999999999999, 6544565445646546])
+    expect(subject.show_numbers(2, text_with_numbers)).to eq([999999999999999999999999999999999999, 6544565445646546])
   end
+
+  it ".show_numbers shows message: 'No numbers!'" do
+    expect(subject.show_numbers(text_without_numbers)).to eq("No numbers!")
+  end
+
+  it ".show_numbers shows error message: 'n' is Non-integer" do
+    expect(subject.show_numbers('a', text_with_numbers)).to eq("Error: parameter 'number' must be Integer!")
+  end
+
 end
